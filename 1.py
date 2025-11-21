9160c46cdaee254961a895a71cc10bdb92dc8b10
Prassan6 <rastogiprassan6@gmail.com>
2025-11-21 12:42:56 +0530

    made repo changes

--- 1.py
+++
@@ -1,9 +0,0 @@
-n= int(input("enter a number")
-if n%2==0
-print("Not Weird")
-else if n in range(2,5)
-print("Not Weird")
-else if n in range(6,20)
-print("Weird")
-else if n%2==0 and n>20
-print("Not Weird")
